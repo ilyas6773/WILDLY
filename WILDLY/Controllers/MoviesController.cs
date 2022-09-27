985c9d91e8362776479b0cce80846ed95c87a8c4
ilyas6773 <mister.tolegenov@yandex.ru>
2022-09-27 22:57:29 +0600

    feat: the navigation bar now contains Movies and Customers displaying corresponding items. Also some cosmetic changes

--- WILDLY/Controllers/MoviesController.cs
+++ WILDLY/Controllers/MoviesController.cs
@@ -1,18 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using WILDLY.Models;
 using WILDLY.ViewModels;
 
 namespace WILDLY.Controllers
 {
-    public class MovieController : Controller
+    public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { id = 1, Name = "Shrek" },
+                new Movie { id = 2, Name = "Wall-e" }
+            };
+        }
         // GET: Movie
         public ActionResult Random()
         {
+
+            
             var movie = new Movie()
             {
                 Name = "Shrek!"
